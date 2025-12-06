77f7d511f10cce72ff20d4596a7fca90918eaa12
Cresswell <cresswelllew@gmail.com>
2025-12-06 13:53:00 +0800

    done setup

---
+++ includes/config.example.php
@@ -0,0 +1,6 @@
+<?php
+
+define('DB_SERVER', 'YOUR_SERVER_NAME');
+define('DB_DATABASE', 'YOUR_DB_NAME');
+define('DB_USER', 'YOUR_USER');
+define('DB_PASSWORD', 'YOUR_PASSWORD');
---
+++ includes/db.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/config.php';
+
+function getDbConnection()
+{
+    $connectionInfo = [
+        "Database" => DB_DATABASE,
+        "UID"      => DB_USER,
+        "PWD"      => DB_PASSWORD,
+    ];
+
+    $conn = sqlsrv_connect(DB_SERVER, $connectionInfo);
+    
+    if ($conn === false) {
+        die("Database connection failed.");
+    }
+
+    return $conn;
+}
